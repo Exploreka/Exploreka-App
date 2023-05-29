d4ef46a545d224c0ff64b4255b11cfdc2c8ae0ed
Muhammad Izzudin <mohammadzzudin@gmail.com>
2023-05-29 13:58:22 +0700

    fix error splash screen

--- app/src/main/java/com/exploreka/app/SplashScreenActivity.kt
+++ app/src/main/java/com/exploreka/app/SplashScreenActivity.kt
@@ -3,15 +3,18 @@ package com.exploreka.app
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
+import android.widget.ImageView
 
 class SplashScreenActivity : AppCompatActivity() {
-    private val SPLASH_DELAY: Long = 3000
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
         setContentView(R.layout.activity_splash_screen)
-        Handler().postDelayed({
+
+        val SPLASH_DELAY: Long = 3000
+        val imageView: ImageView = findViewById(R.id.splash)
+
+        imageView.postDelayed({
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
             finish()
