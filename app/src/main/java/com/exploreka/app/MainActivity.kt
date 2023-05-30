b84fc68544ffd76c69aaa2f93d733bfb3f2c9eb5
ardian-arif-wahyudi <ardiarif72@gmail.com>
2023-05-30 22:17:10 +0700

    Update Main Activity

--- app/src/main/java/com/exploreka/app/MainActivity.kt
+++ app/src/main/java/com/exploreka/app/MainActivity.kt
@@ -1,11 +1,40 @@
 package com.exploreka.app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var bottomNavigationView: BottomNavigationView
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation)
+        bottomNavigationView.selectedItemId = R.id.menu_home
+        bottomNavigationView.setOnNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.menu_home -> {
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                    true
+                }
+                R.id.menu_wishlist -> {
+                    val intent = Intent(this, WishlistActivity::class.java)
+                    startActivity(intent)
+                    true
+                }
+                R.id.menu_profile -> {
+                    val intent = Intent(this, ProfileActivity::class.java)
+                    startActivity(intent)
+                    true
+                }
+                else -> false
+            }
+        }
     }
 }
