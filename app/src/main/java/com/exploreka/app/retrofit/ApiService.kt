18cc956403cfbda6b9d807e1c29a5280070dd050
ardian-arif-wahyudi <ardiarif72@gmail.com>
2023-06-12 19:22:26 +0700

    Update Api

--- app/src/main/java/com/exploreka/app/retrofit/ApiService.kt
+++ app/src/main/java/com/exploreka/app/retrofit/ApiService.kt
@@ -3,41 +3,28 @@ package com.exploreka.app.retrofit
 import com.exploreka.app.data.response.login.LoginResponse
 import com.exploreka.app.data.response.main.AttractionsResponse
 import com.exploreka.app.data.response.register.RegisterResponse
+import com.exploreka.app.retrofit.model.ModelAttraction
+import com.exploreka.app.retrofit.model.ModelTourPackage
 import okhttp3.MultipartBody
 import okhttp3.RequestBody
 import retrofit2.http.*
 
 interface ApiService {
 
-    @POST("login")
-    @FormUrlEncoded
-    suspend fun login(
-        @Field("email") email: String,
-        @Field("password") password: String
-    ) : LoginResponse
+    @GET("attractions")
+    suspend fun getAttractions(): List<ModelAttraction>
 
-    @POST("register")
-    @FormUrlEncoded
-    suspend fun register(
-        @Field("fullname") fullname: String,
-        @Field("email") email: String,
-        @Field("password") password: String,
-        @Field("confPassword") confPassword: String
-    ) : RegisterResponse
+    @GET("tour_package")
+    suspend fun getTourPackage(): List<ModelTourPackage>
 
     @GET("attractions")
-    suspend fun getAttractions(
-        @Query("id_attraction") id_attraction: Int,
-        @Query("name_attraction") name_attraction: String,
-        @Query("desc_attraction") desc_attraction: String,
-        @Query("photo_attraction") photo_attraction: String,
-        @Query("coordinate_attraction") coordinate_attraction: String,
-        @Query("rating_avg_attraction") rating_avg_attraction: String,
-        @Query("open_hour") open_hour: String,
-        @Query("close_hour") close_hour: String,
-        @Query("id_attraction_cat") id_attraction_cat: Int,
-        @Query("id_city") id_city: Int
-    ): AttractionsResponse
+    suspend fun getAttractions(): List<ModelAttraction>
+
+    @GET("tour_package")
+    suspend fun getTourPackage(): List<ModelTourPackage>
+
+
+
 
 
 
