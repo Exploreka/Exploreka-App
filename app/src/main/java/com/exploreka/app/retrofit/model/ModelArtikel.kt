1488cc9f1357f84622998e09bdb6a2f906872f13
ardian-arif-wahyudi <ardiarif72@gmail.com>
2023-06-13 22:50:30 +0700

    Add Artikel

--- app/src/main/java/com/exploreka/app/retrofit/model/ModelArtikel.kt
+++ app/src/main/java/com/exploreka/app/retrofit/model/ModelArtikel.kt
@@ -4,78 +4,22 @@ import com.google.gson.annotations.SerializedName
 
 data class ModelArtikel(
 
-    @field:SerializedName("kind")
-    val kind: String? = null,
+    @field:SerializedName("createdAt")
+    val createdAt: String? = null,
 
-    @field:SerializedName("author")
-    val author: Author? = null,
+    @field:SerializedName("headline_post")
+    val headlinePost: String? = null,
 
-    @field:SerializedName("published")
-    val published: String? = null,
+    @field:SerializedName("content_post")
+    val contentPost: String? = null,
 
-    @field:SerializedName("blog")
-    val blog: Blog? = null,
+    @field:SerializedName("id_post")
+    val idPost: Int? = null,
 
-    @field:SerializedName("title")
-    val title: String? = null,
+    @field:SerializedName("image_post")
+    val imagePost: String? = null,
 
-    @field:SerializedName("url")
-    val url: String? = null,
+    @field:SerializedName("updatedAt")
+    val updatedAt: String? = null
 
-    @field:SerializedName("content")
-    val content: String? = null,
-
-    @field:SerializedName("selfLink")
-    val selfLink: String? = null,
-
-    @field:SerializedName("labels")
-    val labels: List<String?>? = null,
-
-    @field:SerializedName("replies")
-    val replies: Replies? = null,
-
-    @field:SerializedName("etag")
-    val etag: String? = null,
-
-    @field:SerializedName("id")
-    val id: String? = null,
-
-    @field:SerializedName("updated")
-    val updated: String? = null
-)
-
-data class Replies(
-
-    @field:SerializedName("totalItems")
-    val totalItems: String? = null,
-
-    @field:SerializedName("selfLink")
-    val selfLink: String? = null
-)
-
-data class Author(
-
-    @field:SerializedName("image")
-    val image: Image? = null,
-
-    @field:SerializedName("displayName")
-    val displayName: String? = null,
-
-    @field:SerializedName("id")
-    val id: String? = null,
-
-    @field:SerializedName("url")
-    val url: String? = null
-)
-
-data class Blog(
-
-    @field:SerializedName("id")
-    val id: String? = null
-)
-
-data class Image(
-
-    @field:SerializedName("url")
-    val url: String? = null
 )
